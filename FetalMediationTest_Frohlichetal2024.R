ac550676425183b36474520233744b8b58b9c934
Joel Frohlich <52724317+jfrohlich@users.noreply.github.com>
2024-01-05 10:36:10 +0100

    Update FetalMediationTest_Frohlichetal2024.R
    added code comments

--- FetalMediationTest_Frohlichetal2024.R
+++ FetalMediationTest_Frohlichetal2024.R
@@ -1,13 +1,15 @@
+# Joel Frohlich
+# University of Tuebingen
+
 rm(list = ls()) # clear all existing vars
 set.seed(111) # seed RNG
-#install.packages("lme4")
-#install.packages("mediation")
-library(lme4)
-library(MASS)
-library("mediation")
+library(lme4) # Loads the 'lme4' package for fitting linear mixed-effects models
+library(MASS) # Loads the 'MASS' package (Modern Applied Statistics with S), which contains statistical functions
+library("mediation") #  Loads the 'mediation' package for conducting mediation analysis
 
-# Read table generated from all patients, including burst suppression
+#  Reads a CSV file containing data into a data frame
 T0 = read.csv("C:/Users/joelf/OneDrive/Documents/Research/Preissl/FetalData.csv",'header'=TRUE)
+
 # Mean center data
 T0$CTW  = scale(T0$CTW, scale=FALSE)
 T0$LZC  = scale(T0$LZC, scale=FALSE)
@@ -19,6 +21,18 @@ T0$mval = scale(T0$mval, scale=FALSE)
 
 nsm = 1000 # number of simulations to run
 useme = T0 # data table to use
+
+# Mediation Analysis for Each Variable:
+#
+#     For each entropy measure (LZC, CTW, PE32, PE64, MSE, SE):
+#         Model Setup:
+#             XpredY <- lmer(...): Fits a linear mixed-effects model predicting the variable from sex (XChrom) with random intercepts for ID.
+#             XMpredY <- lmer(...): Fits a similar model but includes mean signal amplitudee (mval) as a mediator along with sex (XChrom).
+#         Mediation Analysis:
+#             med <- mediate(...): Conducts a mediation analysis using the specified models and settings (e.g., treat="XChrom", mediator="mval").
+#         Results Summary:
+#             summary(med): Prints a summary of the mediation analysis results for the specific variable.
+
 
 # LZC 
 XpredY <- lmer(LZC ~ XChrom + (1|ID),data=useme) 
